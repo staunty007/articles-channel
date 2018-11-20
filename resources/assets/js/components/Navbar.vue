0a9f9b0118c638dccb0aa66a317fd1e1e78f67fb
Stanley Ezonkeme <dev.staunty@gmail.com>
2018-11-20 03:11:25 +0100

    first commit

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+	<nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+		<div class="conatiner">
+			<a href="#" class="navbar-brand">Larticles</a>
+		</div>
+	</nav>
+</template>
